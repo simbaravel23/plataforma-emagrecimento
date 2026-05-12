a039119c392caaf99ca6928ba635281d8a28dcb9
Ravel Silva <seu.violinavel@hotmail.com>
2026-05-12 20:12:42 -0300

    home app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Aula from './pages/Aula';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/aula/:id" element={<Aula />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
