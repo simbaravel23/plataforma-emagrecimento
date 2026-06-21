a493205ba6028c0f5a8cd08b708854e744a5c1f8
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2026-06-21 20:18:12 -0300

    Atualizar o Home.jsx
    meu depoimento

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -152,7 +152,7 @@ export default function Home() {
               <div className="h-48 rounded-2xl overflow-hidden border border-white/10 group-hover:border-white/50 transition-all duration-500 shadow-lg">
                 <iframe 
                   className="w-full h-full"
-                  src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                  src=https:"//www.youtube.com/embed/G0YT9-_tT98" 
                   title="Depoimento 1"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
